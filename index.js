97f82b4dfededc88f0e3c913ccbebcfe8a246c8f
Sahil Tamboli <sahiltamboli303@gmail.com>
2025-05-07 01:47:55 +0530

    Complete changes for Drive Management Module. API for add/delete/update vaccines_drives

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const PORT = process.env.PORT || 5000;
 // Import routes
 const authRoutes    = require('./routes/auth');
 const studentRoutes = require('./routes/students');
+const driveRoutes = require('./routes/drives');
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', authRoutes);
 app.use('/api/students', studentRoutes);
+app.use('/api/drives', driveRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
